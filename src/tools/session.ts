ca1399b614537e2a7c2bac925767b6835da9bc48
Kylejeong2 <kylejeong21@gmail.com>
2025-08-16 18:01:17 -0700

    rm publish, fix required deps

--- src/tools/session.ts
+++ src/tools/session.ts
@@ -108,7 +108,7 @@ async function handleCreateSession(
         content: [
           {
             type: "text",
-            text: `Browserbase Live Session View URL: https://www.browserbase.com/sessions/${browserbaseSessionId}`,
+            text: `Browserbase Live Session View URL: https://www.browserbase.com/sessions/${session.sessionId}\nBrowserbase Live Debugger URL: ${debugUrl} \n session created`,
           },
           {
             type: "text",
